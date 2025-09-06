43f318ecaa4cc147a6ff8c1a56763a1ae3d86726
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2025-09-06 23:47:41 +0300

    complete exercise 4 by adding the game logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { range } from "../../utils";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ guess }) {
+function Guess({ guess, answer }) {
+  const result = checkGuess(guess, answer);
+
   return (
     <>
       {range(0, NUM_OF_GUESSES_ALLOWED - 1).map((index) => {
         return (
-          <span key={index} className="cell">
+          <span
+            key={index}
+            className={`cell ${guess ? result[index].status : ""}`}
+          >
             {guess ? guess[index] : ""}
           </span>
         );
--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -3,13 +3,13 @@ import { range } from "../../utils";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import Guess from "../Guess/Guess";
 
-function GuessList({ guesses }) {
+function GuessList({ guesses, answer }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((index) => {
         return (
           <p key={guesses[index] ? guesses[index].id : index} className="guess">
-            <Guess guess={guesses[index]?.value} />
+            <Guess guess={guesses[index]?.value} answer={answer} />
           </p>
         );
       })}
